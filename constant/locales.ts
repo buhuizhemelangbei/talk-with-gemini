21f13919e730daf6ed8d504309a14f3881409721
buhuizhemelangbei <153802970+buhuizhemelangbei@users.noreply.github.com>
2024-07-12 23:21:37 +0800

    Update locales.ts

--- constant/locales.ts
+++ constant/locales.ts
@@ -1,3 +1,13 @@
 export default {
+  'ar-SA': 'العربية',
+  'de-DE': 'Deutsch',
+  'en-US': 'English',
+  'es-ES': 'Español',
+  'fr-FR': 'Français',
+  'ja-JP': '日本語',
+  'ko-KR': '한국어',
+  'pt-BR': 'Português',
+  'ru-RU': 'Русский',
   'zh-CN': '简体中文',
-} as const
+  'zh-TW': '繁體中文',
+} as cons
